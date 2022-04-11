f9b10d42f4ed3f156349e4475cc4f78996bebca1
Ismail Tawil <itawil165@gmail.com>
2022-04-11 01:25:59 -0500

    Initial commit

---
+++ src/ignoreMe/IgnoreMe.java
@@ -0,0 +1,8 @@
+package ignoreMe;
+
+public class IgnoreMe {
+    public static void main(String[] args) {
+
+        System.out.println("IGNORE THIS!!!");
+    }
+}
---
+++ src/myGitHubPush/README.md
@@ -0,0 +1,3 @@
+This file is to test branch creation, .gitignore, and README.md creation on GitHub in the MyGitProject repository.
+
+Ismail Tawil
